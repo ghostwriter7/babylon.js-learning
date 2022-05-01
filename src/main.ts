d99199dea95b3b6d20826ad219c6bb334b06e313
Hubert Rozplochowski <hubert.rozplochowski@gmail.com>
2022-05-01 20:14:10 +0200

    Basic scene rendered

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,22 @@
-import './style.css'
+import * as BABYLON from 'babylonjs';
+const canvasEl = document.getElementById('app') as HTMLCanvasElement;
 
-const app = document.querySelector<HTMLDivElement>('#app')!
+const engine = new BABYLON.Engine(canvasEl, true, { preserveDrawingBuffer: true, stencil: true });
 
-app.innerHTML = `
-  <h1>Hello Vite!</h1>
-  <a href="https://vitejs.dev/guide/features.html" target="_blank">Documentation</a>
-`
+const createScene = () => {
+  const scene = new BABYLON.Scene(engine);
+  const camera = new BABYLON.ArcRotateCamera('camera', -Math.PI / 2, Math.PI / 2.5, 3, new BABYLON.Vector3(0, 0, 0), scene);
+  camera.attachControl(canvasEl, true);
+  const light = new BABYLON.HemisphericLight('light', new BABYLON.Vector3(0, 1, 0), scene);
+  const box = BABYLON.MeshBuilder.CreateBox('box', {}, scene);
+
+  return scene;
+}
+const scene = createScene();
+engine.runRenderLoop(() => {
+  scene.render();
+});
+
+window.addEventListener('resize', () => {
+  engine.resize();
+});
